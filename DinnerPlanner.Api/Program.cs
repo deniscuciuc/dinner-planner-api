3194c393e60b5ba2753592874f265e6f8cb446ef
Denis Cuciuc <personal.dcuciuc@gmail.com>
2024-04-26 21:43:51 +0300

    Add global error handling via middleware strategy

--- DinnerPlanner.Api/Program.cs
+++ DinnerPlanner.Api/Program.cs
@@ -1,22 +1,21 @@
+using DinnerPlanner.Api.Middleware;
 using DinnerPlanner.Application;
 using DinnerPlanner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+
     builder.Services.AddControllers();
 }
 
 var app = builder.Build();
-
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+{
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
