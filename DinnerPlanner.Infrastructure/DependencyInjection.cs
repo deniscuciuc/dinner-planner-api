db89ccdac5b82035292b8cdcb375b7e549da3dfd
Denis Cuciuc <personal.dcuciuc@gmail.com>
2024-04-26 14:22:23 +0300

    Implement JWT token generation on pseudo register request

--- DinnerPlanner.Infrastructure/DependencyInjection.cs
+++ DinnerPlanner.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DinnerPlanner.Application.Common.Interfaces.Authentication;
+using DinnerPlanner.Application.Common.Interfaces.Services;
+using DinnerPlanner.Infrastructure.Authentication;
+using DinnerPlanner.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DinnerPlanner.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
         return services;
     }
 }
---
+++ DinnerPlanner.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using DinnerPlanner.Application.Common.Interfaces.Services;
+
+namespace DinnerPlanner.Infrastructure.Services;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
